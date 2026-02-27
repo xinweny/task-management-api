f95f310b1ae864458c94048df9e772095911fa88
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-02-27 13:47:10 +0800

    initial setup

---
+++ src/test/java/com/slic/task_management_api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slic.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
