1e13db022749c141696b5fe02e5def19f58e57c5
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-03-02 13:31:05 +0800

    add correct annotation for taskId

--- src/main/java/com/slic/task_management_api/controller/TaskController.java
+++ src/main/java/com/slic/task_management_api/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class TaskController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ResponseDto<?>> deleteTask(
         @AuthenticationPrincipal User user,
-        @RequestBody Long taskId
+        @PathVariable Long taskId
     ) {
         taskService.deleteTask(taskId);
 
