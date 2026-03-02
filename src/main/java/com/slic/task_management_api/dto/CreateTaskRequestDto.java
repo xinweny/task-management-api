8d620a4b82aaa21423ec46df49a802eedcc30994
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-03-02 14:45:28 +0800

    add validation

--- src/main/java/com/slic/task_management_api/dto/CreateTaskRequestDto.java
+++ src/main/java/com/slic/task_management_api/dto/CreateTaskRequestDto.java
@@ -1,8 +1,10 @@
 package com.slic.task_management_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateTaskRequestDto {
+    @NotBlank
     private String title;
 }
