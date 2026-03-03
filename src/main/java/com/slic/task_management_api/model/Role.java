824cc9e4fbff429e7c197e411ac49ce31a54ef2d
Xin Wen Yap <yapxinwen1608@gmail.com>
2026-03-03 21:48:36 +0800

    add index for role names

--- src/main/java/com/slic/task_management_api/model/Role.java
+++ src/main/java/com/slic/task_management_api/model/Role.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,7 +18,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "roles")
+@Table(
+    name = "roles",
+    indexes = {
+        @Index(name = "idx_name", columnList = "name")
+    }
+)
 @Getter
 @Builder
 @AllArgsConstructor
